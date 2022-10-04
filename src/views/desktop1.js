1a7c22fb89fe4a6f12c98f829879699af28e91ad
Fikri Haikal <82983170+PikriNtr@users.noreply.github.com>
2022-10-04 11:09:38 +0800

    Commit from TeleportHQ

---
+++ src/views/desktop1.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './desktop1.css'
+
+const Desktop1 = (props) => {
+  return (
+    <header className="desktop1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="desktop1-desktop1">
+        <img
+          alt="Rectangle2213"
+          src="/playground_assets/rectangle2213-o76n-200h.png"
+          className="desktop1-rectangle2"
+        />
+        <img
+          alt="Vectarytexture27"
+          src="/playground_assets/vectarytexture27-r75-1200h.png"
+          className="desktop1-vectarytexture"
+        />
+        <span className="desktop1-text">
+          <span>Nyoba Web Design</span>
+        </span>
+        <span className="desktop1-text02">
+          <span>Home</span>
+        </span>
+        <span className="desktop1-text04">
+          <span>My Github</span>
+        </span>
+        <span className="desktop1-text06">
+          <span>About Me</span>
+        </span>
+        <img
+          alt="Rectangle129"
+          src="/playground_assets/rectangle129-78gd-2100h.png"
+          className="desktop1-rectangle1"
+        />
+        <img
+          alt="Line1210"
+          src="/playground_assets/line1210-qenj.svg"
+          className="desktop1-line1"
+        />
+        <img
+          alt="Line2211"
+          src="/playground_assets/line2211-t2mh.svg"
+          className="desktop1-line2"
+        />
+        <img
+          alt="Line3212"
+          src="/playground_assets/line3212-8bak.svg"
+          className="desktop1-line3"
+        />
+        <img
+          alt="Rectangle3214"
+          src="/playground_assets/rectangle3214-b58a-200w.png"
+          className="desktop1-rectangle3"
+        />
+        <span className="desktop1-text08">
+          <span>
+            <span>Anggap aja ini text buat ngisi anu nya</span>
+            <br></br>
+            <span>klo udh jadi yaaa kyknya bkln di lanjutin projectnya</span>
+          </span>
+        </span>
+        <span className="desktop1-text13">
+          <span>si paling jago design</span>
+        </span>
+        <span className="desktop1-text15">
+          <span>PikriNtr Web</span>
+        </span>
+        <img
+          alt="Rectangle4219"
+          src="/playground_assets/rectangle4219-5n1-200h.png"
+          className="desktop1-rectangle4"
+        />
+        <span className="desktop1-text17">
+          <span>login</span>
+        </span>
+        <span className="desktop1-text19">
+          <span>tanya Fikri klo mw tw login cridentials nya</span>
+        </span>
+      </div>
+    </header>
+  )
+}
+
+export default Desktop1
